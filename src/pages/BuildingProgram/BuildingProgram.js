d8349b982b0e777c93d420faf6a4942f01f22774
aprct <fei@aprct.io>
2021-10-30 17:20:57 -0700

    update building program numbers

--- src/pages/BuildingProgram/BuildingProgram.js
+++ src/pages/BuildingProgram/BuildingProgram.js
@@ -9,10 +9,10 @@ import Profile from '../../components/Profile';
 import AplosModal from '../../components/AplosModal';
 
 const PROGRESS_VALUES = {
-  annualProgress: 61451,
+  annualProgress: 67434,
   annualGoal: 100000,
-  total: 570198,
-  monthsRemaining: 4,
+  total: 576425,
+  monthsRemaining: 3,
   donors: 45
 }
 
